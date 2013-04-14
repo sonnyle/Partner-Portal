07788045e5946ef2df0bfc355e94f13b2f68ea02
Arturo <arturo.diaz@wavesoluciones.com>
2013-04-13 22:39:39 -0700

    Added view for individual accounts

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,27 +1,24 @@
 module AccountsHelper
-	def last_package_version account_id
-		packages = Package__c.find_all_by_Account__c(account_id)
 
+	def packages_by_account account_id
+		packages = Package__c.find_all_by_Account__c(account_id)
+	end
+
+	def last_package_version account_id
 		versions = []
-		packages.each do |package|
+		packages_by_account(account_id).each do |package|
 			versions << package.Version__c.to_f
 		end
 
 		versions.sort!.last
 	end
 
-		def last_package_extensions account_id
-		packages = Package__c.find_all_by_Account__c(account_id)
-
+	def last_package_extensions account_id
 		versions = []
-		packages.each do |package|
+		packages_by_account(account_id).each do |package|
 			versions << package.Extensions__c.to_i
 		end
 
 		versions.sort.last
 	end
-end
-
-def packages_by_account account_id
-	packages = Package__c.find_all_by_Account__c(account_id)
 end
