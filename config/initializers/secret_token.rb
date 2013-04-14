1a1afe9c9a6a25f4f01b21d36fc1d58245faadf7
Arturo <arturo.diaz@wavesoluciones.com>
2013-04-13 19:07:31 -0700

    Clean Slate setup in rails 3.0.9

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Partnerportal::Application.config.secret_token = 'f77831cb762bf7cf11a209fdc06cc6e58b010e69db0e05fc6a1965ff47f9a90a1ff768c84b33434709f02b3ebc489a618643fd86064ca3ddf558d1eebfc86933'
